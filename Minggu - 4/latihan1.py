16d771a53fe24181c63df48d3b9228b0ee9ad5d1
Hafiidh0430 <xian73811@gmail.com>
2025-11-04 15:48:13 +0700

    nambah tugas anjggg

--- Minggu - 4/latihan1.py
+++ Minggu - 4/latihan1.py
@@ -1,39 +1,111 @@
-nama = input("Nama Penyewa: ")
-jenis_ps = input("Jenis PS [PS3/PS4/PS5]: ")
-sewa_perjam = int(input("Harga Sewa [Jam]: "))
-sewa_perhari = int(input("Harga Sewa [Hari]: "))
-# total_transaksi = int(input("Lama Sewa [x]: "))
-
-total_lama, total_harga_perjam, total_harga_perhari = 0, 0, 0
-
+# inisialisasi data harga sewa PS
 data_ps = [
     {"jenis_ps": "PS3", "harga_perjam": 20000, "harga_perhari": 400000},
     {"jenis_ps": "PS4", "harga_perjam": 25000, "harga_perhari": 500000},
     {"jenis_ps": "PS5", "harga_perjam": 30000, "harga_perhari": 600000},
 ]
 
+# inisialisasi total harga jam/hari
+total_harga_perjam, total_harga_perhari = 0, 0
+
+# inisialisasi list data transaksi
 data_transaksi = []
 
-for ps in data_ps:
-   if jenis_ps.lower() == ps['jenis_ps'].lower():
-      if sewa_perjam > 0:
-        if sewa_perjam > 3:
-            total_harga_perjam = (ps['harga_perjam'] * 0.05) * sewa_perjam
-        else:
-            total_harga_perjam = ps['harga_perjam'] * sewa_perjam
-      
-      if sewa_perhari > 0:
-          if sewa_perhari > 3:
-              total_harga_perhari = (ps['harga_perhari'] * 0.15) * sewa_perhari
-          else:
-              total_harga_perhari = ps['harga_perhari'] * sewa_perhari
-          
-print(total_harga_perjam)
-print(total_harga_perhari)
-              
-          
-          
-           
-          
+# inisialisasi variabel sewa lagi
+is_sewa_lagi = "YA"
 
+# inisialisasi diskon dan bonus
+diskon_perjam = 0.05
+diskon_perhari = 0.15
+bonus_jam = 2
 
+# loop selama user ingin menyewa lagi
+while is_sewa_lagi == "YA":
+    
+    # inisialisasi transaksi_ke
+    transaksi_ke = 0
+    
+    # input data penyewa
+    nama = input("Nama Penyewa: ")
+    jenis_ps = input("Jenis PS [PS3/PS4/PS5]: ")
+    
+    # loop data ps dan cocokkan dengan input jenis_ps
+    for ps in data_ps:
+        
+        # jika jenis ps cocok
+        if jenis_ps.upper() == ps['jenis_ps']:
+            
+            # inisialisasi input lama sewa
+            sewa_perjam = int(input("Harga Sewa [Jam]: "))
+            sewa_perhari = int(input("Harga Sewa [Hari]: "))
+            
+            # jika sewa perjam/haru > 0
+            if sewa_perjam > 0:
+                
+                # jika sewa perjam > 3, maka dapat dan hitung diskon
+                if sewa_perjam > 3:
+                   total_harga_perjam = ps["harga_perjam"] * sewa_perjam * (1 - diskon_perjam)
+                   
+                # jika tidak, maka tidak dapat diskon
+                else:
+                    total_harga_perjam = ps['harga_perjam'] * sewa_perjam
+            
+            # jika sewa perhari > 0
+            if sewa_perhari > 0:
+                
+                # jika sewa perhari > 3, maka dapat dan hitung diskon
+                if sewa_perhari > 3:
+                    total_harga_perhari = ps["harga_perhari"] * sewa_perhari * (1 - diskon_perhari)
+                    
+                # jika tidak, maka tidak dapat diskon
+                else:
+                    total_harga_perhari = ps['harga_perhari'] * sewa_perhari
+            
+            # loop dan cek transaksi ke berapa
+            for transaksi in data_transaksi:
+                
+                #  cek, jika nama sama dengan data transaksi sebelumnya
+                if transaksi["nama"].lower() == nama.lower():
+                    
+                    # tambah 1 transaksi_ke
+                    transaksi_ke += 1
+                    
+                    # jika transaksi ke 6/ > 5, maka berikan bonus 2 jam sewa
+                    if transaksi["transaksi_ke"] == 6:
+                        transaksi["sewa_perjam"] += bonus_jam
+                        
+            # jika transaksi ke masih 0, maka set ke 1
+            else:
+                transaksi_ke += 1
+                
+            # simpan data transaksi ke dalam data_transaksi dan break loop
+            data_transaksi.append({
+                "nama": nama,
+                "jenis_ps": jenis_ps,
+                "sewa_perjam": sewa_perjam,
+                "sewa_perhari": sewa_perhari,
+                "total_harga_perjam": total_harga_perjam,
+                "total_harga_perhari": total_harga_perhari,
+                "total_bayar": total_harga_perjam + total_harga_perhari,
+                "transaksi_ke": transaksi_ke,
+            })
+            break
+    # jika jenis ps tidak tersedia, maka ulangi (continue)
+    else:
+        print("Jenis PS tidak tersedia! Coba lagi.")
+        continue  
+         
+    # apakah ingin menyewa lagi? jika iya, maka akan ulangi loop
+    is_sewa_lagi = input("Apakah Ingin Menyewa Lagi? [YA/TIDAK]: ").upper()
+          
+# tampilkan data transaksi
+print("\n==================================== DATA TRANSAKSI ====================================")
+print("No\tNama\t\tJenis PS\tJam\tHari\tTotal/Jam\tTotal/Hari\tTotal Bayar\tTotal/Transaksi-Ke")
+print("==========================================================================================")
+
+# loop dan tampilkan isi data_transaksi
+for i, d in enumerate(data_transaksi, start=1):
+    print(f"{i}\t{d['nama']}\t\t{d['jenis_ps']}\t\t{d['sewa_perjam']}\t{d['sewa_perhari']}\t"
+          f"{d['total_harga_perjam']}\t\t{d['total_harga_perhari']}\t\t{d['total_bayar']}\t\t{d['transaksi_ke']}")
+
+print("==========================================================================================")
---
+++ Minggu - 4/latihan2.py
@@ -0,0 +1,40 @@
+# inisialisasi input range_angka matrix
+range_angka = int(input("Range Angka: "))
+
+# inisialisasi matriks kosong
+matriks = []
+
+# loop baris matrix dengan ordo 3x3
+for i in range(3):
+    
+    # inisialisasi baris kosong
+    baris = []
+    
+    # loop untuk setiap kolom dalam baris
+    for j in range(3):
+        
+        # jika nilai i == j, maka isi nilai = 0
+        if i == j:
+            nilai = 0
+            
+        # jika i < j, maka isi nilai = range_angka + j
+        elif i < j:
+            nilai = range_angka + j
+            
+        # jika tidak keduanya, maka isi nilai = i
+        else:
+            nilai = i
+            
+        # tambahkan nilai ke dalam baris
+        baris.append(nilai)
+        
+    # tambahkan baris ke dalam matriks
+    matriks.append(baris)
+
+# tampilkan hasil matrix
+print("Hasil Matrix:")
+
+# loop setiap baris dalam matriks
+for baris in matriks:
+    # tampilkan baris
+    print(baris)
