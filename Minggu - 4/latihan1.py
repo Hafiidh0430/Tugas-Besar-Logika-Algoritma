7d1ca6f00335e4c7e221bfaea85eb175980bc70f
Hafiidh0430 <xian73811@gmail.com>
2025-11-08 14:42:05 +0700

    update: adding block documentation

--- Minggu - 4/latihan1.py
+++ Minggu - 4/latihan1.py
@@ -17,67 +17,59 @@ is_sewa_lagi = "YA"
 # inisialisasi diskon dan bonus
 diskon_perjam = 0.05
 diskon_perhari = 0.15
+
 bonus_jam = 2
 
+# blok while = fungsi loop while akan berkalan ketika status is_sewa_lagi = "YA" dan akan melakukan proses perhitungan penyewaan
 # loop selama user ingin menyewa lagi
 while is_sewa_lagi == "YA":
-    
     # inisialisasi transaksi_ke
     transaksi_ke = 0
-    
     # input data penyewa
     nama = input("Nama Penyewa: ")
     jenis_ps = input("Jenis PS [PS3/PS4/PS5]: ")
-    
+    # blok for ps = pada fungsi for ini akan melakukan pencocokan jenis ps yang diinput user dengan data ps yang tersedia pada data_ps
     # loop data ps dan cocokkan dengan input jenis_ps
     for ps in data_ps:
-        
+        # blok if jenis_ps = jika jenis ps cocok, maka lakukan proses perhitungan harga sewa
         # jika jenis ps cocok
         if jenis_ps.upper() == ps['jenis_ps']:
-            
             # inisialisasi input lama sewa
             sewa_perjam = int(input("Harga Sewa [Jam]: "))
             sewa_perhari = int(input("Harga Sewa [Hari]: "))
-            
+            # blok sewa_perjam = pada bagian ini akan melakukan perhitungan total harga sewa berdasarkan lama sewa dan diskon yang berlaku
             # jika sewa perjam/haru > 0
             if sewa_perjam > 0:
-                
                 # jika sewa perjam > 3, maka dapat dan hitung diskon
                 if sewa_perjam > 3:
                    total_harga_perjam = ps["harga_perjam"] * sewa_perjam * (1 - diskon_perjam)
-                   
                 # jika tidak, maka tidak dapat diskon
                 else:
                     total_harga_perjam = ps['harga_perjam'] * sewa_perjam
-            
+            # blok sewa_perhari = pada bagian ini akan melakukan perhitungan total harga sewa berdasarkan lama sewa dan diskon yang berlaku
             # jika sewa perhari > 0
             if sewa_perhari > 0:
-                
                 # jika sewa perhari > 3, maka dapat dan hitung diskon
                 if sewa_perhari > 3:
-                    total_harga_perhari = ps["harga_perhari"] * sewa_perhari * (1 - diskon_perhari)
-                    
+                    total_harga_perhari = ps["harga_perhari"] * sewa_perhari * (1 - diskon_perhari) 
                 # jika tidak, maka tidak dapat diskon
                 else:
                     total_harga_perhari = ps['harga_perhari'] * sewa_perhari
-            
+            # blok for transaksi_ke = pada bagian ini akan melakukan pengecekan transaksi ke berapa dari user yang sama
             # loop dan cek transaksi ke berapa
             for transaksi in data_transaksi:
-                
+                # blok if nama = pada bagian ini akan melakukan pengecekan apakah nama penyewa sama dengan data transaksi sebelumnya
                 #  cek, jika nama sama dengan data transaksi sebelumnya
                 if transaksi["nama"].lower() == nama.lower():
-                    
                     # tambah 1 transaksi_ke
                     transaksi_ke += 1
-                    
-                    # jika transaksi ke 6/ > 5, maka berikan bonus 2 jam sewa
+                    # jika transaksi ke 6 atau > 5, maka berikan bonus 2 jam sewa
                     if transaksi["transaksi_ke"] == 6:
                         transaksi["sewa_perjam"] += bonus_jam
-                        
             # jika transaksi ke masih 0, maka set ke 1
             else:
                 transaksi_ke += 1
-                
+            # blok simpan data transaksi = pada bagian ini akan menyimpan semua data transaksi ke dalam list data_transaksi
             # simpan data transaksi ke dalam data_transaksi dan break loop
             data_transaksi.append({
                 "nama": nama,
@@ -94,7 +86,6 @@ while is_sewa_lagi == "YA":
     else:
         print("Jenis PS tidak tersedia! Coba lagi.")
         continue  
-         
     # apakah ingin menyewa lagi? jika iya, maka akan ulangi loop
     is_sewa_lagi = input("Apakah Ingin Menyewa Lagi? [YA/TIDAK]: ").upper()
           
@@ -102,10 +93,9 @@ while is_sewa_lagi == "YA":
 print("\n==================================== DATA TRANSAKSI ====================================")
 print("No\tNama\t\tJenis PS\tJam\tHari\tTotal/Jam\tTotal/Hari\tTotal Bayar\tTotal/Transaksi-Ke")
 print("==========================================================================================")
-
+# blok loop = pada bagian ini akan menampilkan semua data transaksi yang telah disimpan
 # loop dan tampilkan isi data_transaksi
 for i, d in enumerate(data_transaksi, start=1):
     print(f"{i}\t{d['nama']}\t\t{d['jenis_ps']}\t\t{d['sewa_perjam']}\t{d['sewa_perhari']}\t"
           f"{d['total_harga_perjam']}\t\t{d['total_harga_perhari']}\t\t{d['total_bayar']}\t\t{d['transaksi_ke']}")
-
 print("==========================================================================================")
--- Minggu - 4/latihan2.py
+++ Minggu - 4/latihan2.py
@@ -4,30 +4,25 @@ range_angka = int(input("Range Angka: "))
 # inisialisasi matriks kosong
 matriks = []
 
+# blok for i matrix = pada bagian ini akan membuat matrix ordo 3x3 sesuai dengan ketentuan yang diberikan
 # loop baris matrix dengan ordo 3x3
 for i in range(3):
-    
     # inisialisasi baris kosong
     baris = []
-    
+    # blok for j matrix = pada bagian ini akan mengisi setiap kolom dalam baris sesuai dengan ketentuan yang diberikan
     # loop untuk setiap kolom dalam baris
     for j in range(3):
-        
         # jika nilai i == j, maka isi nilai = 0
         if i == j:
             nilai = 0
-            
         # jika i < j, maka isi nilai = range_angka + j
         elif i < j:
             nilai = range_angka + j
-            
         # jika tidak keduanya, maka isi nilai = i
         else:
             nilai = i
-            
         # tambahkan nilai ke dalam baris
         baris.append(nilai)
-        
     # tambahkan baris ke dalam matriks
     matriks.append(baris)
 
